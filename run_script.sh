4a116e615b93782b97427b6d83703341bf572814
ArcticHonour <122731187+Evixityz@users.noreply.github.com>
2024-10-06 22:56:00 +0100

    Add files via upload

---
+++ run_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the directory of this script
+SCRIPT_DIR=$(pwd)
+
+# Set the script name
+SCRIPT_NAME="Israeli_monkey.py"
+
+# Construct the full path to the script
+SCRIPT_PATH="$SCRIPT_DIR/$SCRIPT_NAME"
+
+# Run the script in the background
+nohup python "$SCRIPT_PATH" > /dev/null 2>&1 &
+
