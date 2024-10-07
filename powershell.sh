0f4fb184ae9cfb7eafed99a3e356348a3cf6cb0d
ArcticHonour <122731187+Evixityz@users.noreply.github.com>
2024-10-07 07:34:11 +0100

    Update and rename run_script.sh to powershell.sh

---
+++ powershell.sh
@@ -0,0 +1,14 @@
+# Get the current directory of the script
+$scriptDir = Get-Location
+
+# Set the script name
+$scriptName = "Israeli_monkey.py"
+
+# Construct the full path to the script
+$scriptPath = Join-Path $scriptDir $scriptName
+
+# Give full permission to the Python script
+icacls $scriptPath /grant Everyone:F
+
+# Run the script in the background
+Start-Process "python" -ArgumentList $scriptPath -NoNewWindow -RedirectStandardOutput $null -RedirectStandardError $null
